38e9c1d887a154738006cc771923312873978d1c
Alexander Prokofyev <alexander.prokofyev@skywindgroup.com>
2022-09-09 17:23:16 +0300

    added description

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PromoText from "../PromoText/PromoText";
+import MealList from "../MealList/MealList";
+
+const Meals = () => {
+    return <React.Fragment>
+        <PromoText />
+        <MealList />
+    </React.Fragment>
+}
+
+export default Meals;
